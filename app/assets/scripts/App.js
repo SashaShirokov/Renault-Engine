84a78fec5fedb62ac94b7edefaf8fc98bc0bf95d
Sasha <lyshirokov613@gmail.com>
2017-07-13 21:18:18 +0300

    finished sticky header class

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,11 @@
+import $ from 'jquery';
+
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
+
 
 let mobileMenu = new MobileMenu();
+new RevealOnScroll($('.feature-item'), '85%');
+new RevealOnScroll($('.team'), '60%');
+let stickeHeader = new StickyHeader();
