07c86a6a3a5e8a6e1ffeb85ab27b32ebb757ac56
Sasha <lyshirokov613@gmail.com>
2017-07-14 12:16:52 +0300

    completed modal functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,9 +3,11 @@ import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 
 let mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.team'), '60%');
 let stickeHeader = new StickyHeader();
+let modal = new Modal();
