3b7bf6e32d03b44dd58dd33a98584d1a8c919b6a
Sasha <lyshirokov613@gmail.com>
2017-07-18 11:00:43 +0300

    completed gulp build task

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,7 +5,6 @@ import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 import Modal from './modules/Modal';
 
-
 let mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.team'), '60%');
--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -7,36 +7,40 @@ let cssnano = require('gulp-cssnano');
 let uglify = require('gulp-uglify');
 let browserSync = require('browser-sync').create();
 
-gulp.task('previewDist', function() {
+gulp.task('previewDocs', function() {
   browserSync.init({
     notify: false,
     server: {
-      baseDir: "dist"
+      baseDir: "docs"
     }
   })
 })
 
-gulp.task('deleteDistFolder', function() {
-  return del('./dist');
+gulp.task('deleteDocsFolder', ['icons'], function() {
+  return del('./docs');
 })
 
-gulp.task('optimizeImages', ['deleteDistFolder'], function() {
+gulp.task('optimizeImages', ['deleteDocsFolder'], function() {
   return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
     .pipe(imagemin({
       progressive: true,
       interlaced: true,
       multipass: true
     }))
-    .pipe(gulp.dest('./dist/assets/images'));
+    .pipe(gulp.dest('./docs/assets/images'));
 })
 
-gulp.task('usemin', ['deleteDistFolder'], function() {
+gulp.task('useminTrigger', ['deleteDocsFolder'], function() {
+  gulp.start('usemin');
+})
+
+gulp.task('usemin', ['styles', 'scripts'], function() {
   return gulp.src('./app/index.html')
     .pipe(usemin({
       css: [function() {return rev()}, function() {return cssnano()}],
       js: [function() {return rev()}, function() {return uglify()}]
     }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./docs'));
 })
 
-gulp.task('build', ['deleteDistFolder', 'optimizeImages', 'usemin']);
+gulp.task('build', ['deleteDocsFolder', 'optimizeImages', 'useminTrigger']);
